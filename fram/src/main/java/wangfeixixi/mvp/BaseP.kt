a6fe6b7db8b36b223e86149960d930b00d5d02e3
Fei.Wang20 <xuanyuanxixi@foxmail.com>
2018-07-02 09:36:05 +0800

    减去lib里不用的依赖

--- fram/src/main/java/wangfeixixi/mvp/BaseP.kt
+++
@@ -1,6 +0,0 @@
-package wangfeixixi.mvp
-
-import com.hannesdorfmann.mosby3.mvp.MvpBasePresenter
-import com.hannesdorfmann.mosby3.mvp.MvpView
-
-open class BaseP<V : MvpView> : MvpBasePresenter<V>()
